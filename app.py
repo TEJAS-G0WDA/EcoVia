832fdd75b59d6e46a8e23d93aa87592f68f49a0f
Tejas Gowda <152889993+TEJAS-G0WDA@users.noreply.github.com>
2025-09-13 11:11:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -253,9 +253,14 @@ def charging_stations_api():
 
 	return jsonify({"stations": results})
 
-
 if __name__ == "__main__":
-	app.run(debug=True)
- 
- 
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
+
+
+#if __name__ == "__main__":
+	#app.run(debug=True)
+
+
+
  
